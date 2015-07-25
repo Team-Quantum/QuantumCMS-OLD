105ecd5f5e74f29ef31375f64f02defaee19e72c
DevChuckNorris <dotchucknorris@gmx.de>
2015-07-25 22:11:27 +0200

    Admin Inventory: Basic overlay for item details

--- App/templates/pages/admin/character.tpl
+++ App/templates/pages/admin/character.tpl
@@ -38,7 +38,7 @@
     <br />
     <h3>Inventory</h3>
     <div class="m2-inventory" data-m2-width="5" data-m2-height="9">
-        <div class="m2-item" data-m2-pos="21"></div>
+        <div class="m2-item" data-m2-pos="21" data-m2-vnum="18" data-m2-name="Sword+8"></div>
         <div class="m2-item" data-m2-pos="23"></div>
     </div>
 
--- assets/js/m2.js
+++ assets/js/m2.js
@@ -5,7 +5,17 @@ $(document).ready(function() {
 
         var width = $(this).data('m2-width');
 
-        // Initialise items in
+        var offX = $(this).offset().x;
+        var offY = $(this).offset().y;
+
+        // Create hover div
+        var hover = $('<div>', { class: 'm2-hover' });
+        var title = $('<b>');
+        title.text('No title :(');
+        hover.append(title);
+        $('body').append(hover);
+
+        // Initialise items in inventory
         $(this).find('.m2-item').each(function(index) {
             console.log('item');
 
@@ -13,6 +23,18 @@ $(document).ready(function() {
 
             $(this).css('top', Math.floor(pos / width) * 32);
             $(this).css('left', Math.floor(pos % width) * 32);
+
+            $(this).on('mouseover', function() {
+                title.text($(this).data('m2-name'));
+                hover.css('display', 'block');
+            });
+            $(this).on('mouseout', function() {
+                hover.css('display', 'none');
+            });
+            $(this).on('mousemove', function(evt) {
+                hover.css('left', (evt.pageX + 4) + 'px');
+                hover.css('top', (evt.pageY + 4) + 'px');
+            })
         });
     });
 });
