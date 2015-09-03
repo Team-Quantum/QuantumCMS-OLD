7e49eeb7963a94dff51fd20ae059807ed434a8ca
RealPolluX <jawi785@googlemail.com>
2015-09-03 18:46:30 +0200

    Deletions
    deleted pollux dev settings from index php

--- index.php
+++ index.php
@@ -6,11 +6,6 @@
 
 error_reporting(E_ALL ^ E_NOTICE);
 
-
-// DEVELOPMENT SETTINGS
-ini_set("memory_limit",'2048M');
-ini_set("max_execution_time", '60');
-
 session_name('QuantumSession');
 session_start();
 
